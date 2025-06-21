3aeef4aa425cd6ced42f048563763766dca215b1
rostiSe <150136554+rostiSe@users.noreply.github.com>
2025-06-21 16:08:00 +0200

    Comeon

--- app/api/profile/[id]/route.ts
+++ app/api/profile/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   if (!id) {
